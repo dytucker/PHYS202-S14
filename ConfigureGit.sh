426ea90b744f03721e2dd51a7ea733c2253cb046
Dylan Tucker <dytucker@calpoly.edu>
2014-04-03 15:02:10 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "Dylan Tucker"
+git config --global user.email "dytucker@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
