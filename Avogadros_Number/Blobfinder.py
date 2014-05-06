0a32620db57f2ef434fe7f31aac3ace673b75893
Dylan Tucker <dylan_m_t@yahoo.com>
2014-05-06 10:24:24 -0700

    forgot to save

---
+++ Avogadros_Number/Blobfinder.py
@@ -0,0 +1,13 @@
+class Blob(object):
+	def __init__(self,blob):
+		self.blob = blob
+	def add(i,j)
+	def mass()
+	def distanceTo(blob)
+	centerOfMass()
+
+def BlobFinder(picture, tau):
+
+def countBeads(P):
+
+def getBeads(P):
